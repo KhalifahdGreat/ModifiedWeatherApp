87eef7eccb0830d816743d03370938c19670927b
Onumoh Abubakar <abubakaronumoh@gmail.com>
2024-08-18 12:49:06 +0100

    Initial Commit

---
+++ ModifiedWeatherApp/ModifiedWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiedWeatherAppApp.swift
+//  ModifiedWeatherApp
+//
+//  Created by Khalifah on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ModifiedWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
